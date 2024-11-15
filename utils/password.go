c61e84c8cbd3ca1fb9b3535edbb8ef91848187b1
lawrencesparq <lawrence.kasalwe@sparqtechzm.com>
2024-11-15 23:51:42 +0200

    password validation

--- utils/password.go
+++ utils/password.go
@@ -1,6 +1,12 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -14,4 +20,56 @@ func HashPassword(password string) (string, error) {
 func ComparePasswords(hashedPassword string, plain []byte) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), plain)
 	return err == nil
+}
+
+func IsValidPassword(s string, min int) error {
+	var (
+		isMin   bool
+		special bool
+		number  bool
+		upper   bool
+		lower   bool
+	)
+
+	//test for the minimum characters required for the password string
+	if len(s) < min {
+		isMin = false
+		return fmt.Errorf("length should be atleast " + strconv.Itoa(min) + " characters")
+	}
+
+	for _, c := range s {
+		// Optimize perf if all become true before reaching the end
+		if special && number && upper && lower && isMin {
+			break
+		}
+
+		// else go on switching
+		switch {
+		case unicode.IsUpper(c):
+			upper = true
+		case unicode.IsLower(c):
+			lower = true
+		case unicode.IsNumber(c):
+			number = true
+		case unicode.IsPunct(c) || unicode.IsSymbol(c):
+			special = true
+		}
+	}
+
+	// Add custom error messages
+	if !special {
+		return fmt.Errorf("should contain at least a single special character")
+	}
+	if !number {
+		return fmt.Errorf("should contain at least a single digit")
+	}
+	if !lower {
+		return fmt.Errorf("should contain at least a single lowercase letter")
+	}
+	if !upper {
+		return fmt.Errorf("should contain at least single uppercase letter")
+	}
+
+	// everyting is right
+	return nil
 }
